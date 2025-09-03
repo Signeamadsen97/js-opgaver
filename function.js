c26c8c41b66ae56365b02153a0a739231a5c1762
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2025-09-03 13:35:06 +0200

    Update function.js

--- function.js
+++ function.js
@@ -1,5 +1,5 @@
 //opgave 1
-function sumArray(numbers) {
+function sum(numbers) {
     let sum = 0;
     for (let i = 0; i < numbers.length; i++) {
         sum += numbers[i];
@@ -8,9 +8,35 @@ function sumArray(numbers) {
 }
 
 let tal = [1, 2, 3];
-console.log(sumArray(tal));
+console.log(sum(tal));
 
 
 //opgave 2
-function max(numbers)
-let 
+
+function maxOf(numbers) {
+    return Math.max(...numbers);
+}
+
+const myNumbers = [10, 5, 8, 12, 3];
+const max = maxOf(myNumbers);
+console.log(max);
+
+
+//opgave 3
+
+function countVowels(str) {
+
+    let count = 0;
+    let arr = str.toLowerCase().split("")
+    let vowels = ["e", "i", "o", "u", "y", "æ", "ø", "å"]
+    console.log(arr)
+
+    for (let i = 0; i < str.length; i++) {
+        if (vowels.includes(arr[i])) {
+            count++
+        }
+    }
+    return count
+
+}
+console.log(countVowels('Åge bøjede syv fine rør ud.'))
