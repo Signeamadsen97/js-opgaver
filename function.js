b9a0423afdd4c2fb455d1b4a8786279d2475868c
Signeamadsen97 <119923807+Signeamadsen97@users.noreply.github.com>
2025-09-03 12:59:02 +0200

    Initial commit

---
+++ function.js
@@ -0,0 +1,16 @@
+//opgave 1
+function sumArray(numbers) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+let tal = [1, 2, 3];
+console.log(sumArray(tal));
+
+
+//opgave 2
+function max(numbers)
+let 
